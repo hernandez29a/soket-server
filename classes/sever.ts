8a42588f5e847e8bb5d23be9840a03be20d5dd07
Gustavo Hernandez <hernandez29a@gmail.com>
2022-06-13 14:18:37 -0400

    seccion 4

--- classes/sever.ts
+++ classes/sever.ts
@@ -1,14 +1,51 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server{
+
+    private static _intance: Server;
 
     public app: express.Application;
     public port: number;
 
-    constructor(){
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor(){
        this.app = express();
        this.port = SERVER_PORT;
+       
+       this.httpServer = new http.Server( this.app);
+       this.io = new socketIO.Server( this.httpServer, { cors: { origin: true, credentials: true } } );
+       
+       this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._intance || (this._intance = new this());
+    }
+
+    /**
+     * 
+     * Escuchar los sockets
+     */
+
+    private escucharSockets(){
+        console.log('Escuchando conecxiones - sockets');
+
+        this.io.on('connection' , cliente => {
+            console.log('Cliente Conectado');
+
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+
+            // Desconectar
+            socket.desconectar( cliente );
+        });
+
     }
 
     /**
@@ -16,7 +53,7 @@ export default class Server{
      * Inicializar el Servidor
      */
     start(callback: VoidFunction){
-        this.app.listen( this.port, callback);
+        this.httpServer.listen( this.port, callback);
     }
 
 }
